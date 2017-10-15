4cfcb66f4441d69f71c5e03457eb60b4e95edf53
Sergio Souza <emailsouza@gmail.com>
2017-10-15 15:00:47 -0600

    Added tests to DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -13,17 +13,17 @@ const {Todo} = require('./../models/todo'); // access to the Todo model (based o
 // dummy array of objects
 const todos = [{
   _id: new ObjectID(),
-  text:'First test todo'
+  text: 'First test todo'
 }, {
   _id: new ObjectID(),
-  text:'Second test todo'
+  text: 'Second test todo'
 }];
 
 beforeEach((done) => { // before each time the test is run...
   Todo.remove({}).then(() => { // remove all items from the db
     // done();
-    return Todo.insertMany(todos);  // insert objects from the previously created todos array. return the response to allow chain callbacks
-  }).then(() => done());  // expression syntax - not sure how to explain this
+    return Todo.insertMany(todos); // insert objects from the previously created todos array. return the response to allow chain callbacks
+  }).then(() => done()); // expression syntax - not sure how to explain this
 });
 
 describe('POST /todos', () => { // describe and group the tests. This one is for the POST routes.
@@ -31,7 +31,9 @@ describe('POST /todos', () => { // describe and group the tests. This one is for
     var text = 'Test todo text';
     request(app) // supertest functionalilty. Calls app
       .post('/todos') // specify the request type
-      .send({text}) // sends the data (object). It gets converted to JSON by supertest.  {text} is same as text: text
+      .send({
+        text
+      }) // sends the data (object). It gets converted to JSON by supertest.  {text} is same as text: text
       .expect(200) // assertion
       .expect((res) => { // assert that result (body that comes back) is an object that has the text property specified above
         expect(res.body.text).toBe(text); // expect functionality - https://github.com/mjackson/expect
@@ -40,7 +42,9 @@ describe('POST /todos', () => { // describe and group the tests. This one is for
         if (err) {
           return done(err); // if err, this wraps up the test and send msg to the screen
         }
-        Todo.find({text}).then((todos) => { // fetch the created todo, then callback with a parameter and make assertions with such parameter
+        Todo.find({
+          text
+        }).then((todos) => { // fetch the created todo, then callback with a parameter and make assertions with such parameter
           expect(todos.length).toBe(1); // expect 1 todo exactly, because we add 1 todo item above. (note we delete all items before test is run)
           expect(todos[0].text).toBe(text); // expect object to have a text property just like we specified above
           done();
@@ -49,10 +53,12 @@ describe('POST /todos', () => { // describe and group the tests. This one is for
   });
 
   it('should NOT create todo with invalid body data', (done) => {
-    var text = '';  // text with empty string - should not pass the checks in the todo model
+    var text = ''; // text with empty string - should not pass the checks in the todo model
     request(app)
       .post('/todos')
-      .send({text})
+      .send({
+        text
+      })
       .expect(400)
       .end((err, res) => {
         if (err) {
@@ -81,13 +87,15 @@ describe('GET /todos', () => { // describe and group the tests. This one is for 
   });
 });
 
-describe('GET /todos:id', () => {  // requires a document with an ID - create/add such ID in this test file
+describe('GET /todos:id', () => { // requires a document with an ID - create/add such ID in this test file
   it('should return todo doc', (done) => {
     request(app)
       .get(`/todos/${todos[0]._id.toHexString()}`) // fetch the '_id' object from the todos array, and convert it to a string.
       .expect(200)
       .expect((res) => {
-        expect(res.body.todo.text).toBe(todos[0].text);  // 'todo' is waht comes back. see 'res.send({todo});' in findById function on server.js
+        // 'todo' is waht comes back. see 'res.send({todo});' in findById function on server.js
+        // and we compare it to the todo array text property we created
+        expect(res.body.todo.text).toBe(todos[0].text);
       })
       .end(done);
   });
@@ -103,6 +111,45 @@ describe('GET /todos:id', () => {  // requires a document with an ID - create/ad
   it('should return a 404 for non-objects ids (format/type not valid)', (done) => {
     request(app)
       .get('/todos/123')
+      .expect(404)
+      .end(done);
+  });
+});
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    // 1st item of the todos array created/inserted above, by id (converted object to string)
+    var hexId = todos[1]._id.toHexString();
+    request(app)
+      .delete(`/todos/${hexId}`) // delete the 1st item of the todos array created/inserted above, by id (converted object to string)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId); // expect that the id of the todo that comes back matches the id of the array we created
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);  //
+        }
+        // Query the database using findById toNotExist
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var hexId = new ObjectID().toHexString(); // create a new object id, which will NOT match what's in the todos array (not in the database)
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if object id is invalid', (done) => {
+    var hexId = new ObjectID().toHexString(); // create a new object id, which will NOT match what's in the todos array (not in the database)
+    request(app)
+      .delete(`/todos/${hexId}`)
       .expect(404)
       .end(done);
   });
